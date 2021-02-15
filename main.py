8494a8d0dbb22ff02682f59e4f253ce4949966cb
manglesh456 <58839386+manglesh456@users.noreply.github.com>
2021-02-15 17:17:24 +0530

    new

--- main.py
+++ main.py
@@ -1,46 +1,66 @@
 from PIL import Image 
 from numpy import asarray 
 import json
+import csv
+def wafer(fa,i):
+    row=fa['die']['rows']
+    col=fa['die']['columns']
+    
+    
+    for x in range(row*col):#image number
+        img = Image.open(f'Level_{i}_data/wafer_image_{x+1}.png') 
+        caller(fa,x+1,img)
 
-with open(r'Level_1_data\input.json') as file:
-    f=json.load(file,)
-  
-# load the image and convert into  
-# numpy array 
-img = Image.open('Level_1_data/wafer_image_1.png') 
-numpydata = asarray(img) 
-  
+def caller(f,img_num,img):
+    numpydata = asarray(img).tolist() 
+
+    for i in f['exclusion_zones']:
+        top_left=i['top_left']
+        bottom_right=i['bottom_right']
+        u_left=top_left['x']
+        u_right=bottom_right['x']
+        d_left=top_left['y']
+        d_right=bottom_right['y']
+
+        for i in range(d_right,d_left):
+    
+            cap=dict()
+            for j in range(d_left,u_right-3):
+                numpydata[i][j][0]=-22222
+                numpydata[i][j][1]=-22222
+                numpydata[i][j][2]=-22222
 # data 
 
-x=0
-ans=list()
-for area in f['care_areas']:
-    top_left=area['top_left']
-    bottom_right=area['bottom_right']
-    u_left=top_left['x']
-    u_right=bottom_right['x']
-    d_left=top_left['y']
-    d_right=bottom_right['y']
+    for area in f['care_areas']:
+        top_left=area['top_left']
+        bottom_right=area['bottom_right']
+        u_left=top_left['x']
+        u_right=bottom_right['x']
+        d_left=top_left['y']
+        d_right=bottom_right['y']
 
-    for i in range(d_right,d_left):
+        for i in range(d_right,d_left):
     
-        cap=dict()
-        for j in range(d_left,u_right-3):
-            lt=list()
-            lt.append(numpydata[i][j][0])
-            lt.append(numpydata[i][j][1])
-            lt.append(numpydata[i][j][2])
+            cap=dict()
+            for j in range(d_left,u_right-3):
+                if numpydata[i][j][0]==-22222 and numpydata[i][j][1]==-22222 and numpydata[i][j][2]==-22222:
+                    continue
+                lt=list()
+                lt.append(numpydata[i][j][0])
+                lt.append(numpydata[i][j][1])
+                lt.append(numpydata[i][j][2])
         
-            lt2=list()
-            lt2.append(numpydata[i][j+1][0])
-            lt2.append(numpydata[i][j+1][1])
-            lt2.append(numpydata[i][j+1][2])
+                lt2=list()
+                lt2.append(numpydata[i][j+1][0])
+                lt2.append(numpydata[i][j+1][1])
+                lt2.append(numpydata[i][j+1][2])
 
-            lt3=list()
-            lt3.append(numpydata[i][j+2][0])
-            lt3.append(numpydata[i][j+2][1])
-            lt3.append(numpydata[i][j+2][2])
+                lt3=list()
+                lt3.append(numpydata[i][j+2][0])
+                lt3.append(numpydata[i][j+2][1])
+                lt3.append(numpydata[i][j+2][2])
         
-            if lt==lt3 and lt2!=lt and lt2!=lt3:
-                ans.append([i,j+1])
-print(ans)
+                if lt==lt3 and lt2!=lt and lt2!=lt3:
+                    with open('data.csv','a') as file:
+                        csvwriter=csv.writer(file)
+                        csvwriter.writerow((img_num,j,i))
