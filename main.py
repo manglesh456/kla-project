eb5261e9866356514d8c8cd3b073a795a377397f
manglesh456 <58839386+manglesh456@users.noreply.github.com>
2021-02-15 19:24:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,52 @@
 
-from PIL import Image 
-from numpy import asarray 
 import json
 import csv
-def wafer(fa,i):
-    row=fa['die']['rows']
-    col=fa['die']['columns']
-    
-    
-    for x in range(row*col):#image number
-        img = Image.open(f'Level_{i}_data/wafer_image_{x+1}.png') 
-        caller(fa,x+1,img)
+import cv2
 
-def caller(f,img_num,img):
-    numpydata = asarray(img).tolist() 
+with open('input.json') as s:
+    data = json.load(s)
 
-    for i in f['exclusion_zones']:
-        top_left=i['top_left']
-        bottom_right=i['bottom_right']
-        u_left=top_left['x']
-        u_right=bottom_right['x']
-        d_left=top_left['y']
-        d_right=bottom_right['y']
+import pandas as pd
+from PIL import Image
+import numpy as np
+from numpy import asarray
 
-        for i in range(d_right,d_left):
-    
-            cap=dict()
-            for j in range(d_left,u_right-3):
-                numpydata[i][j][0]=-22222
-                numpydata[i][j][1]=-22222
-                numpydata[i][j][2]=-22222
-# data 
+image1 = Image.open('wafer_image_1.png')
+image2 = Image.open('wafer_image_2.png')
+image3 = Image.open('wafer_image_3.png')
+image4 = Image.open('wafer_image_4.png')
+image5 = Image.open('wafer_image_5.png')
+data[1] = asarray(image1)
+data[2] = asarray(image2)
+data[3] = asarray(image3)
+data[4] = asarray(image4)
+data[5] = asarray(image5)
+Result = []
 
-    for area in f['care_areas']:
-        top_left=area['top_left']
-        bottom_right=area['bottom_right']
-        u_left=top_left['x']
-        u_right=bottom_right['x']
-        d_left=top_left['y']
-        d_right=bottom_right['y']
+image1_ROI= cv2.imread(image1)
+cropped = image1[0:600, 0:800]
 
-        for i in range(d_right,d_left):
-    
-            cap=dict()
-            for j in range(d_left,u_right-3):
-                if numpydata[i][j][0]==-22222 and numpydata[i][j][1]==-22222 and numpydata[i][j][2]==-22222:
-                    continue
-                lt=list()
-                lt.append(numpydata[i][j][0])
-                lt.append(numpydata[i][j][1])
-                lt.append(numpydata[i][j][2])
-        
-                lt2=list()
-                lt2.append(numpydata[i][j+1][0])
-                lt2.append(numpydata[i][j+1][1])
-                lt2.append(numpydata[i][j+1][2])
+dataarr = np.ndarray(shape=(6, 600, 800, 1), dtype=float, order='F')
+for i in range(600):
+    for j in range(800):
+        for k in range(1, 6):
+            dataarr[k][i][j][0] = (data[k][i][j][0] + data[k][i][j][1] + data[k][i][j][2]) / 3
+for l in range(1, 4):
+    for i in range(600):
+        for j in range(0, 800):
+            if (dataarr[l][i][j][0] != dataarr[l + 1][i][j][0]) and (dataarr[l][i][j][0] != dataarr[l + 2][i][j][0]):
+                Result.append([l, i, j])
 
-                lt3=list()
-                lt3.append(numpydata[i][j+2][0])
-                lt3.append(numpydata[i][j+2][1])
-                lt3.append(numpydata[i][j+2][2])
-        
-                if lt==lt3 and lt2!=lt and lt2!=lt3:
-                    with open('output.csv','a') as file:
-                        csvwriter=csv.writer(file)
-                        csvwriter.writerow((img_num,j,i))
+for i in range(600):
+    for j in range(0, 800):
+        if (dataarr[4][i][j][0] != dataarr[5][i][j][0]) and (dataarr[4][i][j][0] != dataarr[3][i][j][0]):
+            Result.append([4, i, j])
+
+for i in range(600):
+    for j in range(0, 800):
+        if (dataarr[4][i][j][0] != dataarr[5][i][j][0]) and (dataarr[5][i][j][0] != dataarr[3][i][j][0]):
+            Result.append([5, i, j])
+
+df = pd.DataFrame(Result, columns=['Die', 'x', 'y'])
+df.to_csv('KLA_ANS.csv', header=False, index=False)
 
